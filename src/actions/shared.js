a6c607d834ef1e88d137aa8e4c3be3600290feed
Vadzim <vadzimk@hotmail.com>
2019-03-13 20:19:53 -0700

    created actioncreator: setAuthedUser

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,11 +1,18 @@
 import {getInitialData} from "../utils/api";
-import {receiveTweets, RECEIVE_TWEETS} from "./tweets";
-import {receiveUsers, RECEIVE_USERS} from "./users";
+import {receiveTweets} from "./tweets";
+import {receiveUsers} from "./users";
+import {setAuthedUser} from "./authedUser";
+
+//assign authUser to mock api
+const AUTH_ID = 'tylermcginnis';
 
 export const handleInitialData =()=>(dispatch)=>{
     return getInitialData().then(({users, tweets})=>{
         //add the users and tweets to the state of the redux store -dispatch a few different actions
         dispatch(receiveUsers(users));
         dispatch(receiveTweets(tweets));
+        dispatch(setAuthedUser(AUTH_ID));
     });
 };
+
+//next will set up reducers to modify state based on these actions
