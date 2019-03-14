caae5ca8bb4e13a0e8e3998d3408099f975ccb9c
Vadzim <vadzimk@hotmail.com>
2019-03-14 01:12:59 -0700

    added loadingBar from react-redux-loading

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,18 +2,21 @@ import {getInitialData} from "../utils/api";
 import {receiveTweets} from "./tweets";
 import {receiveUsers} from "./users";
 import {setAuthedUser} from "./authedUser";
-
+import {showLoading, hideLoading} from 'react-redux-loading';
 //assign authUser to mock api
 const AUTH_ID = 'tylermcginnis';
 
 
 //to support returning a funcion from action creator we need redux-thunk package
 export const handleInitialData =()=>(dispatch)=>{
+    dispatch(showLoading());
+
     return getInitialData().then(({users, tweets})=>{
         //add the users and tweets to the state of the redux store -dispatch a few different actions
         dispatch(receiveUsers(users));
         dispatch(receiveTweets(tweets));
         dispatch(setAuthedUser(AUTH_ID));
+        dispatch(hideLoading());
     });
 };
 
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {connect}from 'react-redux';
 import {handleInitialData} from "../actions/shared";
 import Dashboard from './Dashboard';
+import LoadingBar from 'react-redux-loading';
+
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleInitialData());
@@ -10,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div>
+          <LoadingBar/>
           {this.props.loading===true
               ?null:
               <Dashboard/>}
